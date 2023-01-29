878c5529a2b62bd748ffe6421d4a141aef8ae162
Ekiben542 <71870048+Ekiben542@users.noreply.github.com>
2023-01-29 14:14:39 +0900

    Add files via upload

---
+++ ファイル移動検知.py
@@ -0,0 +1,25 @@
+# coding: shift-jis
+import sys
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+    def on_moved(self, event):
+        if event.is_directory:
+            print("directory moved from {0} to {1}".format(event.src_path,event.dest_path))
+        else:
+            print("file moved from {0} to {1}".format(event.src_path,event.dest_path))
+
+if __name__ == "__main__":
+    path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
